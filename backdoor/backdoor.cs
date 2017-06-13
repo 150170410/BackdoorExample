3d989db66d355628a90b89687e9609ce4c11b370
Kent Green <Kent.Green@xamarin.com>
2017-06-13 16:35:36 -0700

    Update project settings

--- backdoor/backdoor.cs
+++ backdoor/backdoor.cs
@@ -14,7 +14,7 @@ namespace backdoor
                     VerticalOptions = LayoutOptions.Center,
                     Children = {
                         new Label {
-                            XAlign = TextAlignment.Center,
+                            HorizontalTextAlignment = TextAlignment.Center,
                             Text = "Welcome to Xamarin Forms!"
                         }
                     }
